8f27be26d9bef1f905f128bfc5996cd63d29d9d9
mjjd <mjjdaldrine@gmail.com>
2024-11-30 16:00:55 +0300

    uo

--- includes/header.php
+++ includes/header.php
@@ -6,12 +6,12 @@
     <nav class="navbar">
         <ul>
             <li><a href="index.php">الصفحة الرئيسية</a></li>
-            <li><a href="services.php">الخدمات</a></li>
-            <li><a href="shop.php">المتجر</a></li>
+            <li><a href="/pages/services.php">الخدمات</a></li>
+            <li><a href="/pages/shop.php">المتجر</a></li>
             <li><a href="/pages/feedback.php">الشكاوى</a></li>
-            <li><a href="suggestions.php">الاقتراحات</a></li>
-            <li><a href="contact.php">التواصل</a></li>
-            <li><a href="video.php">احدث الملابس </a></li>
+            <li><a href="/pages/suggestions.php">الاقتراحات</a></li>
+            <li><a href="/pages/contact.php">التواصل</a></li>
+            <li><a href="/pages/video.php">احدث الملابس </a></li>
             <li><a href="../pages/login.php">التسجيل</a></li>
         </ul>
     </nav>
