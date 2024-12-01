61e6306ca6c1d6c3c651b0c70993ff5906bbb89f
mjjd <mjjdaldrine@gmail.com>
2024-12-01 14:10:10 +0300

    u

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,18 @@
 <!-- footer.php -->
 <footer>
-<head>
-<link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <nav>
         <ul>
             <li>شكراً على زيارتكم موقعنا التجاري</li>
-            
         </ul>
     </nav>
-    </head>
+    <!-- إضافة صور W3C validation -->
+    <div class="w3c-validation">
+        <a href="https://validator.w3.org/check?uri=referer" target="_blank">
+            <img src="https://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!" />
+        </a>
+        <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+            <img src="https://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+        </a>
+    </div>
 </footer>
